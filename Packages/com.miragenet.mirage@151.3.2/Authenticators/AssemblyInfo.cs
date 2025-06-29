148018f92b8f818e94e1615de40c582bdd4ee13e
DESKTOP-H3H2056\25jordan <jordam25@gmail.com>
2025-06-29 22:16:32 +0900

    mirage embed 추가

---
+++ Packages/com.miragenet.mirage@151.3.2/Authenticators/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("151.3.2")]
+
+[assembly: InternalsVisibleTo("Mirage.Tests.Runtime")]
---
+++ Packages/com.miragenet.mirage@151.3.2/Runtime/SocketLayer/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("151.3.2")]
+
+[assembly: InternalsVisibleTo("Mirage.SocketLayer.Tests")]
